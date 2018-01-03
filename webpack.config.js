b578b7297d8f049e0f27b2eb53827709467b205d
Damian Mullins <damian.mullins@just-eat.com>
2018-01-03 16:18:19 +0000

    v1.0.0 - Release version of f-templates.

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    target: 'node',
+
+    entry: './src/templates.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'templates.js',
+        libraryTarget: 'commonjs'
+    },
+
+    module: {
+        loaders: [
+            {
+                exclude: /node_modules/,
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+
+    resolve: {
+        alias: {
+            handlebars: 'handlebars/dist/handlebars.js'
+        }
+    }
+};
