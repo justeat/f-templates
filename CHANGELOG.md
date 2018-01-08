08e8ddf59d087047ec77fabb454ae927ef8c8131
Damian Mullins <damian@lowflyingowls.co.uk>
2018-01-08 10:42:59 +0000

    CPD-1452 - Added i18n helper and partial support.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,15 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+v0.0.2
+------------------------------
+*January 08, 2018*
+
+### Added
+- Added support for internationalisation.
+- Added support for partial views.
+
+
 v0.0.1
 ------------------------------
 *January 04, 2018*
--- src/templates.js
+++ src/templates.js
@@ -1,24 +1,56 @@
 const fs = require('fs');
 const path = require('path');
 const handlebars = require('handlebars');
+const { i18n } = require('handlebars-helper-i18n');
 const glob = require('glob');
+
+handlebars.registerHelper('i18n', i18n);
+
+
+const loadPartials = templatePath => {
+    // /path/to/module/{moduleName}/partials/*.hbs
+    const partialsDir = path.resolve(templatePath, 'partials');
+    const filenames = fs.readdirSync(partialsDir);
+
+    filenames.forEach(filename => {
+        const matches = /^([^.]+).hbs$/.exec(filename);
+
+        if (!matches) {
+            return;
+        }
+
+        const name = matches[1]; // eslint-disable-line prefer-destructuring
+        const template = fs.readFileSync(`${partialsDir}/${filename}`, 'utf8');
+
+        handlebars.registerPartial(name, template);
+    });
+};
+
 
 module.exports = (callback, moduleName, options) => {
     const globPattern = '**/templates';
     const ignore = '**/node_modules/**';
     const globOptions = { ignore };
 
-    glob(globPattern, globOptions, (err, files) => {
+    glob(globPattern, globOptions, (err, [result]) => {
         if (err) {
             callback(err);
         }
 
-        const [result] = files;
+        // /path/to/module/{moduleName}/index.hbs
+        const templatePath = path.resolve(process.cwd(), result, moduleName);
+        const rawTemplate = fs.readFileSync(`${templatePath}/index.hbs`, 'utf-8');
+        const template = handlebars.compile(rawTemplate);
 
-        const templatePath = path.resolve(process.cwd(), result, `${moduleName}.hbs`);
-        const source = fs.readFileSync(templatePath, 'utf-8');
-        const compiled = handlebars.compile(source);
+        loadPartials(templatePath);
 
-        callback(null, compiled(options));
+        // /path/to/module/{moduleName}/resources/{moduleName}.json
+        const translationsPath = path.resolve(process.cwd(), result, `resources/${moduleName}.json`);
+        const rawTranslations = fs.readFileSync(translationsPath, 'utf-8');
+        const translations = JSON.parse(rawTranslations);
+
+        const opts = Object.assign({}, translations, options);
+
+        callback(null, template(opts));
     });
 };
